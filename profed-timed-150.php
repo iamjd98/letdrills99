f290f273803380b0e3245ebd52d854f22b610154
Jan Denver Gimenez <iamjd98@gmail.com>
2023-06-28 15:09:59 +0800

    Changes to be committed:

--- profed-timed-150.php
+++ profed-timed-150.php
@@ -10,7 +10,7 @@ include 'session.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/gened-prem.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>LET DRILLS 99</title>
+    <title>Prof Ed 150 Questions - Time Limit</title>
 </head>
 <body>
 
--- profed-timed-menu.php
+++ profed-timed-menu.php
@@ -11,7 +11,7 @@ include 'session.php';
     <link rel="stylesheet" href="./styles/gened-menu.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>LET DRILLS 99</title>
+    <title>Prof Ed Menu - Time Limit</title>
 </head>
 <body>
 
